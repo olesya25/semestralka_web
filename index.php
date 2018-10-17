ad7cd4096e9ac81e03fc250d9674fd74eb20c78f
Olesya Dudchuk <leskadudchuk@gmail.com>
2018-10-17 23:52:41 +0200

    pripojeni k databazi pomoci PDO.

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<h1>Plan your training</h1><hr>
+<!--Pripojeni k databazi-->
+<?php include_once 'resource/Database.php';?>
+
+<P>Haven't created a diary yet? <a href="signup.php">Create now!</a> Already have your own diary? <a href="login.php">Login</a></P>
+
+
 
 </body>
-</html>-->
+</html>
 
-<?php
-include 'resource/Database.php';
-
-$conn = OpenCon();
-
-//echo "Connected Successfully";
-
-CloseCon($conn);
-
-?>
 
 
--- resource/Database.php
+++ resource/Database.php
@@ -1,1 +1,30 @@
-<?php/** * Created by PhpStorm. * User: olesya * Date: 15/10/2018 * Time: 21:16 */function OpenCon(){    $dbhost = "students.kiv.zcu.cz";    $dbuser = "db1_vyuka";    $dbpass = "db1_vyuka";    $db = "db1_vyuka";    //$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);    $conn = mysqli_connect($dbhost, $dbuser, $dbpass);    if ($conn)        echo 'Success connect';    else        die('Connection failed');//    if($conn) echo "Connected to the register database";    return $conn;}function CloseCon($conn){    mysqli_close($conn);    echo '\rConnection was successfully closed';}?>
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: olesya
+ * Date: 15/10/2018
+ * Time: 21:16
+ */
+
+
+
+$dsn = 'mysql:host = localhost; dbname=workout_diary';
+$dbuser = 'root';
+$dbpass = 'mysql';
+
+try{
+    $db = new PDO($dsn, $dbuser, $dbpass);
+
+    echo "Connected Successfully";
+
+}catch (Exception $exception) {
+
+    echo "Connection failed" . $exception->getMessage();
+}
+
+
+?>
+
+
+
